8cb9b68123f70669056c439a892042b41aca9562
Subhas Dandapani <rdsubhas@gmail.com>
2017-10-28 12:13:10 +0200

    Use SITE_FQDN if present

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 OmniAuth.config.logger = Rails.logger
+OmniAuth.config.full_host = ENV['SITE_FQDN'] if ENV['SITE_FQDN']
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET']
 end
