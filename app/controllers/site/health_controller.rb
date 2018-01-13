b79fbf5e5f023c851686c1933c587b4c72a3fd1a
Lorenzo Fundaro <lorenzo.fundaro@goeuro.com>
2018-01-13 12:55:06 +0100

    refactor health check

--- app/controllers/site/health_controller.rb
+++ app/controllers/site/health_controller.rb
@@ -1,14 +1,13 @@
 module Site
   class HealthController < BaseController
     def show
-      app_client = Github::GithubClient.instance.new_app_client
       begin
-        app_client.find_app_installations()
+        Github::GithubClient.instance.new_app_client.find_app_installations
+        render json: {status: "OK"}, status: 200
       rescue => e
-        logger.error 'Connection to Github failed'
-        return render json: {"status": "Connection to Github failed"}, status: 500
+        logger.error 'Connection to Github failed', e
+        render json: {status: "Connection to Github failed"}, status: 500
       end
-      render json: {status: "OK"}, status: 200
     end
   end
 end
