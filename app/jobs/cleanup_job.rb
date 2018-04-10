3ca139d0d09b451cc9f950cf35d398af864697c3
Subhas Dandapani <rdsubhas@gmail.com>
2018-04-10 23:33:53 +0200

    Move to namespace

--- app/jobs/cleanup_job.rb
+++
@@ -1,8 +0,0 @@
-class CleanupJob < ApplicationJob
-  queue_as :default
-
-  def perform(*args)
-    # Remove events older than 30 days
-    Event.where("created_at < ?", 30.days.ago).delete_all
-  end
-end
